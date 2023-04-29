f9853c6e597a8edab8433be961d3bf67f5036c25
Raj Gopinath <125770743+learnvarsity@users.noreply.github.com>
2023-04-29 18:55:41 -0500

    Add files via upload

---
+++ basic/variables2.py
@@ -0,0 +1,20 @@
+'''
+Name: variable1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: Variables
+'''
+v1 = input("Enter a number")
+v1 = float(v1) 
+v2 = input("Enter another number")
+v2 = float(v2)
+total = v1+v2
+print("The sum of ", v1, " and ", v2, " is ", total)
+
+
+'''
+Exercise - 1:
+
+Input 3 floats
+
+'''
