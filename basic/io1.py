01f9c4e4d0be7e53caea1ba8ba9d1226c5bee5cf
Raj Gopinath <125770743+learnvarsity@users.noreply.github.com>
2023-04-29 18:46:37 -0500

    Add files via upload

---
+++ basic/io1.py
@@ -0,0 +1,20 @@
+'''
+Name: io1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: Basic input/output
+'''
+
+
+#basic input
+name=input("Please enter your name: ")
+age=input("Please enter your age: ")
+
+#basic output
+print("You entered your name as ",name, "your age is ",age)
+
+'''
+Exercise - 1:
+Input your zodiac and age and display
+
+'''
---
+++ basic/variables1.py
@@ -0,0 +1,12 @@
+'''
+Name: variable1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: Variables
+'''
+v1 = input("Enter a number")
+v1 = int(v1) 
+v2 = input("Enter another number")
+v2 = int(v2)
+total = v1+v2
+print("The sum of ", v1, " and ", v2, " is ", total)
