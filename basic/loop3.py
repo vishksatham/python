282fba2b777e1be8b655bda3b784096773edbe55
Raj Gopinath <125770743+learnvarsity@users.noreply.github.com>
2023-04-29 19:12:01 -0500

    Add files via upload

---
+++ basic/loop3.py
@@ -0,0 +1,19 @@
+'''
+Name: loop1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: odd numbers and even numbers
+'''
+i=1
+while i<=10:
+    print(i)
+    i=i+2
+
+i=0
+while i<=10:
+    print(i)
+    i=i+2
+
+'''
+Exercise: print odd numbers, even numbers
+'''
---
+++ basic/loop4.py
@@ -0,0 +1,16 @@
+'''
+Name: loop1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: 12th table
+'''
+i=1
+while i<=12:
+    answer = 12*i
+    print("12 * ", i, " = ",  answer)
+    i=i+1
+
+
+'''
+Exercise: 13th table
+'''
