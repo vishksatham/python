7efade213485dc7aabf0aa84f47f560a27661a8d
Raj Gopinath <125770743+learnvarsity@users.noreply.github.com>
2023-04-29 19:06:15 -0500

    Add files via upload

---
+++ basic/loop1.py
@@ -0,0 +1,14 @@
+'''
+Name: loop1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: positive series
+'''
+i=0
+while i<=10:
+    print(i)
+    i=i+1
+
+'''
+Exercise: print odd numbers, even numbers
+'''
---
+++ basic/loop2.py
@@ -0,0 +1,14 @@
+'''
+Name: loop1.py
+Author: Raj Gopinath
+Date: 04/29/2023 
+Purpose: negative series
+'''
+i=-10
+while i<=0:
+    print(i)
+    i=i+1
+
+'''
+Exercise: print odd numbers, even numbers
+'''
