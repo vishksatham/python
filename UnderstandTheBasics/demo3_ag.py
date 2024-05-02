d0596915f3a3b7d06edafbad969ed4c697638cb7
ASHWIN <90372256+Ashwin0132@users.noreply.github.com>
2024-05-02 21:17:16 +0530

    Create demo3_ag.py

---
+++ UnderstandTheBasics/demo3_ag.py
@@ -0,0 +1,12 @@
+'''CELSIUS TO FAHRENHEIT'''
+
+c = float(input("Enter the centigrade: "))
+f = (1.8 * c  + 32)
+print( "The Fahrenheit is = ", f)
+
+
+'''FAHRENHEIT TO CELSIUS'''
+
+f = float(input("Enter the fahrenheit: "))
+c = (f-32) / 1.8
+print("Celsius = ",c)
