6744d4387b6c1edafd41a859e9cbf484a0d6acb4
vishksatham <108156196+vishksatham@users.noreply.github.com>
2024-05-05 18:52:21 -0700

    Create demo3_vs.py

---
+++ UnderstandTheBasics/demo3_vs.py
@@ -0,0 +1,23 @@
+"""
+Learn about lists, tuples, and dictionaries.
+Understand how to manipulate these data structures (e.g., appending to a list, updating dictionary values).
+"""
+
+nums = [] 
+
+while True:
+
+    userIn = input("Enter an integer (input 'done' to finish): ")
+    
+    if(userIn != 'done'):
+
+        num = int(userIn) 
+        nums.append(num)
+
+    elif userIn == 'done':
+        break
+   
+nums.sort(reverse = True)
+nums_tuple = tuple(nums)
+
+print("Numbers in descending order: " + str(nums_tuple))
