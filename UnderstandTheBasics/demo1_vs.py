88e7e0f3549c79059c8ffaa238363e3748f74b91
vishksatham <vishksatham@gmail.com>
2024-05-05 18:47:41 -0700

    DemoExercises
    INDEX 1 - 3

---
+++ UnderstandTheBasics/demo1_vs.py
@@ -0,0 +1,19 @@
+"""
+Learn about variables, data types (integers, floats, strings, booleans), and basic operations (+, -, *, /, //, %, **).
+Explore how to print output using the print() function.
+Familiarize yourself with basic input using the input() function.
+"""
+
+number = int(input("Enter a number: "))
+
+factorial = int(number)
+
+if(number <= 0):
+    print("Factorial cannot be computed")
+
+else:
+    for x in range(1, number):
+
+        factorial = factorial * x
+
+    print("The factorial of your input is: " + str(factorial))
